d59af5ec2f38668b7cc887bc69c4914227fab960
darKKOREO <tanapat.daoloy@mail.kmutt.ac.th>
2022-09-09 21:05:44 +0700

    1st_commit

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,22 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  let stw = startWord
+  let w = word
+
+   if(w ===null || w === undefined) {
+    return undefined
+  }
+   else if(w.startsWith('first')){
+    return word
+  }else{
+    return stw+w
+  }
+
+
+
+
+
+
 }
 module.exports = fillStartWord
